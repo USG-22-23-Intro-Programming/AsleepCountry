fc93240eeea0cacc8e8a66f11a2fa363c92a7de9
Eisha Ahmad <eisha_ahmad@m018464.local>
2022-10-26 22:45:56 -0700

    problemSet2

---
+++ problemSet2.py
@@ -0,0 +1,31 @@
+def factorial(x): 
+    
+    y=1
+
+    for i in range(1, x + 1):
+
+        y = y * i
+
+    print ("factorial" + str(x) + " okWhat")
+    print (y)
+
+def DoubleIt():
+    word = input("please enter a phrase you would like to Double")
+    output= ""
+    for char in word:
+        output = output + char * 2
+    print(output)
+
+
+def main():
+    factorial(5)
+    factorial(3)
+
+    DoubleIt()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
